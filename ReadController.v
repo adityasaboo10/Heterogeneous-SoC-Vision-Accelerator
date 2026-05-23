af741967dbdcdfc0d1d3043666127041549073ca
Aditya Saboo <102903261+adityasaboo10@users.noreply.github.com>
2026-05-23 17:59:18 +0530

    Refactor vec_valid output to wire type

--- ReadController.v
+++ ReadController.v
@@ -9,7 +9,7 @@ module ReadController#(parameter PIXW = 8,parameter K_size = 3, parameter LB_siz
                   output reg all_strides_done,
                   output reg [4:0] base_pos,
                   output reg [(K_size * K_size * PIXW)-1:0] ifMAP_flat,  //output to vec engine
-                  output reg vec_valid  //-> to en of vector engine
+                  output vec_valid  //-> to en of vector engine
                   );
 
 //-----------------------
@@ -22,12 +22,13 @@ localparam buf4 = 2'd3;
 reg [4:0] rd_counter;
 reg reading_active;  //set high when rd_Start is high
 
+assign vec_valid = (rst == 1)? reading_active : 0 ;
+
 always @(posedge clk_50M or negedge rst) begin
     if(!rst) begin
         base_pos         <= 0;
         all_strides_done <= 0;
         rd_counter       <= 0;
-        vec_valid        <= 0;
         reading_active   <= 0;
         ifMAP_flat       <= 0;
     end
@@ -35,7 +36,6 @@ always @(posedge clk_50M or negedge rst) begin
         base_pos         <= 0;
         all_strides_done <= 0;
         rd_counter       <= 0;
-        vec_valid        <= 0;
         reading_active   <= 0;
         ifMAP_flat       <= 0;
     end
@@ -45,11 +45,10 @@ always @(posedge clk_50M or negedge rst) begin
         if(rd_start) reading_active <= 1'b1;
 
         if(reading_active) begin
-            if(vec_done) begin
+            if(vec_done) begin   //done signal from vector engine
                 if(rd_counter < LB_size-K_size) begin
                     base_pos   <= base_pos + 1;
                     rd_counter <= rd_counter + 1;
-                    vec_valid  <= 1'b1;
                     case(lb_state)
                         buf1: ifMAP_flat <= {out_pix2, out_pix3, out_pix4};
                         buf2: ifMAP_flat <= {out_pix3, out_pix4, out_pix1};
@@ -58,7 +57,6 @@ always @(posedge clk_50M or negedge rst) begin
                     endcase
                 end
                 else begin
-                    vec_valid        <= 1'b0;
                     all_strides_done <= 1'b1;
                     rd_counter       <= 0;
                     base_pos         <= 0;
@@ -67,7 +65,6 @@ always @(posedge clk_50M or negedge rst) begin
             end
             else begin
                 if(rd_counter == 0 ) begin
-                    vec_valid <= 1'b1;
                     case(lb_state)
                         buf1: ifMAP_flat <= {out_pix2, out_pix3, out_pix4};
                         buf2: ifMAP_flat <= {out_pix3, out_pix4, out_pix1};
@@ -75,7 +72,6 @@ always @(posedge clk_50M or negedge rst) begin
                         buf4: ifMAP_flat <= {out_pix1, out_pix2, out_pix3};
                     endcase
                 end
-                else vec_valid <= 1'b0;
             end
         end
     end
