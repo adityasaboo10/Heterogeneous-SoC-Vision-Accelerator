17f6671552d07471765840285673a51901061549
Aditya Saboo <102903261+adityasaboo10@users.noreply.github.com>
2026-05-23 21:28:15 +0530

    Refactor ReadController for reset and reading logic

--- ReadController.v
+++ ReadController.v
@@ -22,59 +22,49 @@ localparam buf4 = 2'd3;
 reg [4:0] rd_counter;
 reg reading_active;  //set high when rd_Start is high
 
-assign vec_valid = (rst == 1)? reading_active : 0 ;
+assign vec_valid = (rst && master_start)? (reading_active) : 0 ;
 
 always @(posedge clk_50M or negedge rst) begin
-    if(!rst) begin
-        base_pos         <= 0;
-        all_strides_done <= 0;
-        rd_counter       <= 0;
-        reading_active   <= 0;
-        ifMAP_flat       <= 0;
-    end
-    else if(!master_start) begin
-        base_pos         <= 0;
-        all_strides_done <= 0;
-        rd_counter       <= 0;
-        reading_active   <= 0;
-        ifMAP_flat       <= 0;
-    end
-    else begin
-        all_strides_done <= 1'b0;
+if(!rst) begin
+    base_pos <= 0;
+    all_strides_done <= 0;
+    rd_counter <= 0;
+    reading_active <= 0;
+    ifMAP_flat <= 0;
+end
+else if(!master_start) begin
+    base_pos         <= 0;
+    all_strides_done <= 0;
+    rd_counter       <= 0;
+    reading_active   <= 0;
+    ifMAP_flat       <= 0;
+end
+else begin
+    all_strides_done <= 1'b0;
 
-        if(rd_start) reading_active <= 1'b1;
-
-        if(reading_active) begin
-            if(vec_done) begin   //done signal from vector engine
-                if(rd_counter < LB_size-K_size) begin
-                    base_pos   <= base_pos + 1;
-                    rd_counter <= rd_counter + 1;
-                    case(lb_state)
-                        buf1: ifMAP_flat <= {out_pix2, out_pix3, out_pix4};
-                        buf2: ifMAP_flat <= {out_pix3, out_pix4, out_pix1};
-                        buf3: ifMAP_flat <= {out_pix4, out_pix1, out_pix2};
-                        buf4: ifMAP_flat <= {out_pix1, out_pix2, out_pix3};
-                    endcase
-                end
-                else begin
-                    all_strides_done <= 1'b1;
-                    rd_counter       <= 0;
-                    base_pos         <= 0;
-                    reading_active   <= 1'b0;
-                end
-            end
-            else begin
-                if(rd_counter == 0 ) begin
-                    case(lb_state)
-                        buf1: ifMAP_flat <= {out_pix2, out_pix3, out_pix4};
-                        buf2: ifMAP_flat <= {out_pix3, out_pix4, out_pix1};
-                        buf3: ifMAP_flat <= {out_pix4, out_pix1, out_pix2};
-                        buf4: ifMAP_flat <= {out_pix1, out_pix2, out_pix3};
-                    endcase
-                end
-            end
+    if(reading_active) begin
+        if(rd_counter < LB_size - K_size) begin
+            base_pos   <= base_pos + 1;
+            rd_counter <= rd_counter + 1;
         end
+        else begin
+                all_strides_done <= 1'b1;
+                rd_counter       <= 0;
+                base_pos         <= 0;
+                reading_active   <= 1'b0;
+        end
+    end
     end
 end
 
+always @(*) begin
+    if(rd_start) reading_active <= 1;
+    ifMAP_flat <= 0;
+    case(lb_state)
+        buf1: ifMAP_flat <= {out_pix2, out_pix3, out_pix4};
+        buf2: ifMAP_flat <= {out_pix3, out_pix4, out_pix1};
+        buf3: ifMAP_flat <= {out_pix4, out_pix1, out_pix2};
+        buf4: ifMAP_flat <= {out_pix1, out_pix2, out_pix3};
+    endcase
+end
 endmodule
