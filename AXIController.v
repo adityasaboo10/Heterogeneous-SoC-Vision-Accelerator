6315ad9e6968426853f00b1e8b57e95eb8dc20b7
Aditya Saboo <102903261+adityasaboo10@users.noreply.github.com>
2026-05-25 02:35:24 +0530

    Refactor AXIController module with new parameters

--- AXIController.v
+++ AXIController.v
@@ -1,163 +1,218 @@
-module AXIController #(parameter PIXW = 8,parameter K_size = 3, parameter LB_size = 20)
-                 (input clk_50M,
-                  input rst,
-                  input WVALID1, WVALID2, WVALID3,WVALID4,
-                  input all_strides_done,
-                  input master_start,
-                  output reg rd_start, 
-                  output reg [1:0] lb_state,
-                  output wire write_en1, write_en2, write_en3, write_en4
-                  );
-//--------------------             
-reg [4:0] counter;
-//----------------------
-//----Start State Param---
-reg start_State;
-localparam START = 1'b0;
-localparam BTW = 1'b1;
-//----------------------
-//-----BTW state param----
-localparam buf1 = 2'd0;
-localparam buf2 = 2'd1;
-localparam buf3 = 2'd2;
-localparam buf4 = 2'd3;
-//------------------------------
- reg WREADY1, WREADY2, WREADY3, WREADY4;
- //-------------------
- 
- // Combinational write enables based on AXI handshake
-assign write_en1 = WVALID1 && WREADY1;
-assign write_en2 = WVALID2 && WREADY2;
-assign write_en3 = WVALID3 && WREADY3;
-assign write_en4 = WVALID4 && WREADY4;
+//In the start all 4 LBs fill one by one
+    //After that each row fills with new set of pixels
+    
+    module AXIController #(parameter PIXW = 8,parameter K_size = 3, parameter LB_size = 20)
+                         (input clk_50M,
+                          input rst,
+                          input s_axis_tvalid,
+                          input s_axis_tlast,
+                          input all_strides_done,
+                          input master_start,
+                          input [4:0] pointer1, pointer2, pointer3, pointer4,
+                          output reg rd_start, 
+                          output WREADY1, WREADY2, WREADY3, WREADY4,
+                          output reg [1:0] lb_state,
+                          output wire write_en1, write_en2, write_en3, write_en4
+                          );
+    //---------------
+    
+    reg start_State;
+    localparam START = 1'b0;
+    localparam BTW = 1'b1;
+    //----------------------
+    reg [1:0] pointer_state ;
+    localparam P1 = 2'd0;
+    localparam P2 = 2'd1;
+    localparam P3 = 2'd2;
+    localparam P4 = 2'd3;
+    //-----BTW state param----
+    localparam buf1 = 2'd0;
+    localparam buf2 = 2'd1;
+    localparam buf3 = 2'd2;
+    localparam buf4 = 2'd3;
+    //----------------------
+    reg [1:0] current_row;
+    reg check; 
+    reg row_full;
+    reg math_done;
+    //---------------------
+    // write_en fires when stream is valid and that LB is ready
+    assign write_en1 = s_axis_tvalid && WREADY1 && check;
+    assign write_en2 = s_axis_tvalid && WREADY2 && check;
+    assign write_en3 = s_axis_tvalid && WREADY3 && check;
+    assign write_en4 = s_axis_tvalid && WREADY4 && check;
+    
+    assign WREADY1 = (current_row == 0 && master_start && rst && !row_full) ? 1 : 0 ;
+    assign WREADY2 = (current_row == 1 && master_start && rst && !row_full) ? 1 : 0 ;
+    assign WREADY3 = (current_row == 2 && master_start && rst && !row_full) ? 1 : 0 ;
+    assign WREADY4 = (current_row == 3 && master_start && rst && !row_full) ? 1 : 0 ;
+    
+    
+    always @(posedge clk_50M or negedge rst) begin
+        if(!rst) begin
+            rd_start    <= 1'b0;
+            lb_state    <= buf4;
+            start_State <= START;
+            current_row <= 2'd0;
+            check       <= 1'b0;
+            pointer_state <= P1;
+            row_full    <= 1'b0;
+            math_done   <= 1'b0;
+        end
+        else if (!master_start) begin 
+            rd_start    <= 1'b0;
+            lb_state    <= buf4;
+            start_State <= START;
+            current_row <= 2'd0;
+            check       <= 1'b0;
+            pointer_state <= P1;
+            row_full    <= 1'b0;
+            math_done   <= 1'b0;
+        end
+        else begin
+            if(all_strides_done) math_done <= 1'b1;
+            else if(rd_start) math_done <= 1'b0;
 
-
-always @(posedge clk_50M or negedge rst) begin
-    if(!rst) begin
-        rd_start    <= 1'b0;
-        start_State <= START;
-        lb_state    <= buf4;
-        counter     <= 0;
-        WREADY1     <= 1'b1;
-        WREADY2     <= 1'b1;
-        WREADY3     <= 1'b1;
-        WREADY4     <= 1'b1;
-    end 
-    else if(!master_start) begin
-    // IDLE 
-    WREADY1   <= 1'b0;
-    WREADY2   <= 1'b0;
-    WREADY3   <= 1'b0;
-    WREADY4   <= 1'b0;
-    rd_start  <= 1'b0;
-    end
-    else begin
-        case(start_State)
-            START: begin
-                WREADY1     <= 1'b1;
-                WREADY2     <= 1'b1;
-                WREADY3     <= 1'b1;
-                WREADY4     <= 1'b1;
-                if(counter < LB_size) begin
-                    if(WVALID1 && WREADY1) begin
-                        counter = counter + 1;
-                    end
-                   if(counter == K_size) rd_start <= 1'b1;  //for the first input counter is 1 then for second 2 thus for 3 valid pixels counter = 3
-                    else rd_start <= 1'b0;
-                end
-                else begin
-                    counter     <= 0;
-                    start_State <= BTW;
-                    lb_state <= buf1;
-                    WREADY1     <= 1'b0;
-                    WREADY2     <= 1'b0;
-                    WREADY3     <= 1'b0;
-                    WREADY4     <= 1'b0;
-                end
-            end
-//----------------------------------
-            BTW: begin
-            rd_start <= 1'b0;  // add this at top of BTW case
-                case(lb_state)
-                    buf1: begin
-                        WREADY1 <= 1'b1;
-                        WREADY2 <= 1'b0;
-                        WREADY3 <= 1'b0;
-                        WREADY4 <= 1'b0;
-                        if(counter < LB_size) begin
-                            if(WVALID1) begin
-                                counter   <= counter + 1;
+            case(start_State) 
+                START : begin
+                    check <= 1'b1;
+                    rd_start <= 1'b0;
+                    if(s_axis_tvalid) begin
+                        case(pointer_state) 
+                            P1 : begin
+                                if(pointer1 == LB_size - 1 && WREADY1) begin
+                                    current_row <= 2'd1;
+                                    pointer_state <= P2;
+                                end
+                                else begin
+                                    current_row <= 2'd0;
+                                    pointer_state <= P1;
+                                end
                             end
-                        end
-                        else begin
-                            WREADY1 <= 1'b0;
-                            if(all_strides_done) begin
-                                counter  <= 0;
+                            P2 : begin
+                             if(pointer2 == LB_size - 1 && WREADY2) begin
+                                    current_row <= 2'd2;
+                                    pointer_state <= P3;
+                                end
+                                else begin
+                                    current_row <= 2'd1;
+                                    pointer_state <= P2;
+                                end
+                            end
+                            P3 : begin
+                             if(pointer3 == LB_size - 1 && WREADY3) begin
+                                    current_row <= 2'd3;
+                                    pointer_state <= P4;
+                                    rd_start <= 1'b1;
+                                end
+                                else begin
+                                    current_row <= 2'd2;
+                                    pointer_state <= P3;
+                                end
+                            end 
+                            P4 : begin
+                                if(pointer4 == LB_size - 1 && WREADY4) begin
+                                    start_State <= BTW;
+                                    lb_state <= buf1;
+                                    current_row <= 2'd0;
+                                    rd_start <= 1'b1;  
+                                end
+                                else begin
+                                    current_row <= 2'd3;
+                                    pointer_state <= P4;
+                                end
+                            end
+                        endcase
+                    end
+                end
+                BTW : begin
+                    rd_start <= 1'b0;
+                    if(s_axis_tlast) check <= 1'b0;
+                    
+                    case(lb_state)
+//--------------------------------------
+                        buf1 : begin
+                            if(pointer1 ==  LB_size - 1 && s_axis_tvalid && WREADY1) begin
+                                if(math_done || all_strides_done) begin  //after buffer is filled, if math is also done move on
+                                    current_row <= 2'd1;
+                                    lb_state <= buf2;
+                                    rd_start <= 1'b1;
+                                end
+                                else begin  //math is slower than buf
+                                    row_full <= 1'b1;
+                                end
+                            end
+                            else if(row_full && all_strides_done) begin
+                            // We were paused, math just finished, resume!
+                                row_full <= 1'b0;
+                                current_row <= 2'd1;
                                 lb_state <= buf2;
+                                rd_start <= 1'b1;
                             end
-                        end
-                    end
-//--------------------------------
-                    buf2: begin
-                        WREADY1 <= 1'b0;
-                        WREADY2 <= 1'b1;
-                        WREADY3 <= 1'b0;
-                        WREADY4 <= 1'b0;
-                        if(counter < LB_size) begin
-                            if(WVALID2) begin
-                                counter   <= counter + 1;
+                        end    
+//--------------------------------------------
+                        buf2 : begin
+                            if(pointer2 ==  LB_size - 1 && s_axis_tvalid && WREADY2) begin
+                                if(math_done || all_strides_done) begin  //after buffer is filled, if math is also done move on
+                                    current_row <= 2'd2;
+                                    lb_state <= buf3;
+                                    rd_start <= 1'b1;
+                                end
+                                else begin  //math is slower than buf
+                                    row_full <= 1'b1;
+                                end
                             end
-                        end
-                        else begin
-                            WREADY2 <= 1'b0;
-                            if(all_strides_done) begin
-                                counter  <= 0;
+                            else if(row_full && all_strides_done) begin
+                            // We were paused, math just finished, resume!
+                                row_full <= 1'b0;
+                                current_row <= 2'd2;
                                 lb_state <= buf3;
+                                rd_start <= 1'b1;
                             end
                         end
-                    end
-//-----------------------------
-                    buf3: begin
-                        WREADY1 <= 1'b0;
-                        WREADY2 <= 1'b0;
-                        WREADY3 <= 1'b1;
-                        WREADY4 <= 1'b0;
-                        if(counter < LB_size) begin
-                            if(WVALID3) begin
-                                counter   <= counter + 1;
+//--------------------------------------------
+                        buf3 : begin
+                            if(pointer3 ==  LB_size - 1 && s_axis_tvalid && WREADY3) begin
+                                if(math_done || all_strides_done) begin  //after buffer is filled, if math is also done move on
+                                    current_row <= 2'd3;
+                                    lb_state <= buf4;
+                                    rd_start <= 1'b1;
+                                end
+                                else begin  //math is slower than buf
+                                    row_full <= 1'b1;
+                                end
                             end
-                        end
-                        else begin
-                            WREADY3 <= 1'b0;
-                            if(all_strides_done) begin
-                                counter  <= 0;
+                            else if(row_full && all_strides_done) begin
+                            // We were paused, math just finished, resume!
+                                row_full <= 1'b0;
+                                current_row <= 2'd3;
                                 lb_state <= buf4;
+                                rd_start <= 1'b1;
                             end
                         end
-                    end
-//----------------------------
-                    buf4: begin
-                        WREADY1 <= 1'b0;
-                        WREADY2 <= 1'b0;
-                        WREADY3 <= 1'b0;
-                        WREADY4 <= 1'b1;
-                        if(counter < LB_size) begin
-                            if(WVALID4) begin
-                                counter   <= counter + 1;
+//--------------------------------------------
+                        buf4 : begin
+                            if(pointer4 ==  LB_size - 1 && s_axis_tvalid && WREADY4) begin
+                                if(math_done || all_strides_done) begin  //after buffer is filled, if math is also done move on
+                                    current_row <= 2'd0;
+                                    lb_state <= buf1;
+                                    rd_start <= 1'b1;
+                                end
+                                else begin  //math is slower than buf
+                                    row_full <= 1'b1;
+                                end
                             end
-                        end
-                        else begin
-                            WREADY4 <= 1'b0;
-                            if(all_strides_done) begin
-                                counter  <= 0;
+                            else if(row_full && all_strides_done) begin
+                            // We were paused, math just finished, resume!
+                                row_full <= 1'b0;
+                                current_row <= 2'd0;
                                 lb_state <= buf1;
+                                rd_start <= 1'b1;
                             end
-                        end
-                    end
-                endcase
-            end
-        endcase
+                        end                                                
+                    endcase
+                end
+            endcase
+        end
     end
-end
-
-endmodule
+    endmodule
