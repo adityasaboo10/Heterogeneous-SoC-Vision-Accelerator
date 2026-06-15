30f618d49a6e98f6bee9ed2f6b3402cd88323ed2
Aditya Saboo <102903261+adityasaboo10@users.noreply.github.com>
2026-06-15 19:10:36 +0530

    Update PE module to handle signed multiplication

--- PE.v
+++ PE.v
@@ -5,13 +5,19 @@ module PE #(parameter PIXW = 8,
             parameter Output_Width = 32)(
             input clk_50M, 
             input rst,
-            input [PIXW-1 :0] a,
-            input [PIXW-1 :0] b,
+            input [PIXW-1 :0] a, //Unsigned Pixel
+            input [PIXW-1 :0] b, // Signed Weight
             input [Output_Width-1 :0] c,
             input en,
             output reg done,
             output reg [Output_Width-1 :0] out
     );
+// --- THE 9-BIT PADDING TRICK ---
+    wire signed [PIXW   :0] a_signed = {1'b0, a}; // Force to positive
+    wire signed [PIXW-1:0] b_signed = b;         // Read as signed
+    
+// Natively handle the signed multiplication
+    wire signed [Output_Width-1:0] mult_result = a_signed * b_signed;
     
 always @(posedge clk_50M or negedge rst) begin
     if(!rst || !en) begin  //en should be 1 to start computing
@@ -19,7 +25,7 @@ always @(posedge clk_50M or negedge rst) begin
         done <= 1'b0;
     end
     else begin
-        out <= (a*b) + c;
+        out <= mult_result + $signed(c);
         done <= 1'b1;
     end
 end
